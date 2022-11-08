d5464c2cdfde12a9636bb3140e7edca649567499
Théodore L'Heureux <theodorelheureux@gmail.com>
2022-11-08 13:12:00 -0500

    feat: add multithreading

--- Setup.ps1
+++ Setup.ps1
@@ -25,25 +25,22 @@ $GitUserName = "Théodore L'Heureux"
 $GitEmail = "theodorelheureux@gmail.com"
 
 #Skip some questions when executing script ? 0 if false, 1 if true.
-$AlwaysSkipNetSupportUninstall = "0"
-$AlwaysSkipVNCViewerInstall = "0"
+$AlwaysSkipNetSupportUninstall = "1"
+$AlwaysSkipVNCViewerInstall = "1"
 $AlwaysSkipCmderInstall = "0"
+$AlwaysSkipRustInstall = "0"
 $AlwaysSkipVSCodeExtensionUninstall = "0"
 
 $AlwaysUninstallNetSupport = "0"
 $AlwaysInstallVNCViewer = "0"
 $AlwaysInstallCmder = "1"
+$AlwaysInstallRust = "1"
 $AlwaysUninstallVSCodeExtension = "0"
 
-$AskForMouseSpeed = "1"
+$AskForMouseSpeed = "0"
 $ChangeKeyboardLayoutToUs = "1"
 
 #endregion
-
-if ($ChangeKeyboardLayoutToUs -eq "1") {
-	Set-WinUserLanguageList -LanguageList en-US -Force
-	Write-Host '[Done] Set keyboard layout to en-US.'
-}
 
 #region Mouse sensitivity
 function Set-Mouse() {
@@ -76,6 +73,11 @@ function Set-Mouse() {
 }
 #endregion
 
+if ($ChangeKeyboardLayoutToUs -eq "1") {
+	Set-WinUserLanguageList -LanguageList en-US -Force
+	Write-Host '[Done] Set keyboard layout to en-US.'
+}
+
 #region Registry parameters 
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value $AppsUseLightTheme
 Write-Host '[Done] Set AppsUseLightThem to ' -NoNewline
@@ -100,11 +102,11 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 Write-Host '[Done] Set SystemUsesLightTheme to ' -NoNewline
 Write-Host $SystemUsesLightTheme
 
-$Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 Set-ItemProperty -Path $Path -Name "HideIcons" -Value $HideIcons
 Write-Host '[Done] Make desktop icons hidden or visible'
 
-Get-Process "explorer"| Stop-Process
+Get-Process "explorer" | Stop-Process
 Write-Host '[Done] Restart windows explorer'
 #endregion
 
@@ -112,8 +114,104 @@ Write-Host '[Done] Restart windows explorer'
 $Screen = Read-Host -Prompt 'Is monitor ok ? (1/0)'
 
 if ($Screen -eq "0") {
-	Z:\ExtraSoftware\multimonitortool-x64\MultiMonitorTool.exe /SetNextPrimary
+	\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\multimonitortool-x64\MultiMonitorTool.exe /SetNextPrimary
 	Write-Host '[Done] Change main monitor'
+}
+#endregion
+
+#region Start Rust Job
+$RustDone = $false;
+
+if ($AlwaysSkipRustInstall -eq "0") {
+
+	$Screen = "0"
+
+	if ($AlwaysInstallRust -eq "1") {
+		$Screen = "1"
+	}
+	else {
+		$Screen = Read-Host -Prompt 'Install Rust ? (1/0)'
+	}
+
+	if ($Screen -eq "1") {
+		Start-Job -Name rustinstall -Scriptblock {
+			\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\vs_Enterprise.exe modify --installPath "C:\Program Files\Microsoft Visual Studio\2022\Enterprise" --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --quiet --norestart
+			\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\rustup-init.exe -y -q
+		}
+	}
+	else {
+		Write-Host '[Skipped] Install Rust'
+		$RustDone = $true;
+	}
+}
+else {
+	Write-Host '[Skipped] Install Rust'
+	$RustDone = $true;
+}
+#endregion
+
+#region VS Code remove extension
+$VScodeDone = $false;
+
+if ($AlwaysSkipVSCodeExtensionUninstall -eq "0") {
+
+	$Screen = "0"
+
+	if ($AlwaysUninstallVSCodeExtension -eq "1") {
+		$Screen = "1"
+	}
+	else {
+		$Screen = Read-Host -Prompt 'Uninstall useless VSCode extensions ? (1/0)'
+	}
+
+	if ($Screen -eq "1") {
+		Start-Job -Name removeVS -Scriptblock { 
+			Remove-Item "C:\Program Files\Microsoft VS Code\data\extensions\*" -Recurse -Force -ErrorAction SilentlyContinue
+			Copy-Item -Path "\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\extensions.zip" -Destination "C:\Program Files\Microsoft VS Code\data"
+			Start-Process "C:\Program Files\7-Zip\7zG.exe" -ArgumentList "x C:\Program Files\Microsoft VS Code\data\extensions.zip -oC:\Program Files\Microsoft VS Code\data\" -Wait 
+		}
+
+		Write-Host '[Done] Uninstall useless VSCode extensions'
+	}
+	else {
+		Write-Host '[Skipped] Uninstall useless VSCode extensions'
+		$VScodeDone = $true;
+	}
+}
+else {
+	Write-Host '[Skipped] Uninstall useless VSCode extensions'
+	$VScodeDone = $true;
+}
+#endregion
+
+#region Start Cmder Job
+$cmderDone = $false;
+
+if ($AlwaysSkipCmderInstall -eq "0") {
+
+	$Screen = "0"
+
+	if ($AlwaysInstallCmder -eq "1") {
+		$Screen = "1"
+	}
+	else {
+		$Screen = Read-Host -Prompt 'Install Cmder ? (1/0)'
+	}
+
+	if ($Screen -eq "1") {
+		Start-Job -Name cmder -Scriptblock {
+			Copy-Item -Path "\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\cmder.zip" -Destination "C:\Users\2032422\Documents" 
+			Start-Process "C:\Program Files\7-Zip\7zG.exe" -ArgumentList "x C:\Users\2032422\Documents\cmder.zip -oC:\" -Wait
+		}
+	}	
+	else {
+		Write-Host '[Skipped] Install Cmder'
+		$cmderDone = $true;
+	}
+}
+else {
+	Write-Host '[Skipped] Install Cmder'
+	$cmderDone = $true;
 }
 #endregion
 
@@ -126,6 +224,21 @@ git config --global user.email $GitEmail
 Write-Host '[Done] Set git email to "' -NoNewline
 Write-Host $GitEmail -NoNewline
 Write-Host '"'
+
+$gitDone = $false;
+
+Start-Job -Name gitsetup -Scriptblock {
+	Start-Process msiexec.exe -Wait -ArgumentList '/i "\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\GnuPG.msi" /q'
+	
+			$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+			$oldpath += ";C:\Program Files (x86)\gnupg\bin"
+			Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $oldpath
+}
+#endregion
+
+#region Change default browser
+\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\SetDefaultBrowser\SetDefaultBrowser.exe Edge delay=1000
+Write-Host '[Done] Change default browser to Edge'
 #endregion
 
 #region Uninstall NetSupport School
@@ -141,7 +254,7 @@ if ($AlwaysSkipNetSupportUninstall -eq "0") {
 	}
 
 	if ($Screen -eq "1") {
-		msiexec /x "Z:\ExtraSoftware\2b30d.msi"
+		msiexec /x "\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\2b30d.msi"
 		Write-Host '[Done] Uninstall NetSupport School'
 	}
 	else {
@@ -166,7 +279,7 @@ if ($AlwaysSkipVNCViewerInstall -eq "0") {
 	}
 
 	if ($Screen -eq "1") {
-		msiexec /i "Z:\ExtraSoftware\VNC-Viewer\VNC-Viewer-6.21.1109-Windows-en-64bit.msi"
+		msiexec /i "\\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\ExtraSoftware\VNC-Viewer\VNC-Viewer-6.21.1109-Windows-en-64bit.msi"
 		Write-Host '[Done] Install VNC-Viewer'
 	}	
 	else {
@@ -178,104 +291,140 @@ else {
 }
 #endregion
 
-add-appxpackage "Z:\ExtraSoftware\Microsoft.WindowsTerminal_1.10.2383.0_8wekyb3d8bbwe.msixbundle"
+#region Wait for jobs to finish
+$jobsNotDone = $true;
 
-#region VS Code remove extension
-if ($AlwaysSkipVSCodeExtensionUninstall -eq "0") {
+while ($jobsNotDone -eq $true) {
 
-	$Screen = "0"
+	if ($cmderDone -eq $false) {
 
-	if ($AlwaysUninstallVSCodeExtension -eq "1") {
-		$Screen = "1"
+		$cmderStatus = (Get-Job -Name cmder) | Out-String
+
+		if ($cmderStatus.contains("Completed")) {
+			Copy-Item -Path "Z:\ExtraSoftware\settings.json" -Destination "C:\Users\2032422\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\"
+			Write-Host '[Done] Cmder Install'
+			$cmderDone = $true
+		}
+		elseif ($cmderStatus.contains("Running")) {
+			$text = '[Running] Cmder Install'
+			for ($i = 0; $i -lt $text.Length; $i++) {
+				$c = $text[$i]
+		
+				Write-Host $c -NoNewline
+				Start-Sleep -Milliseconds 1
+			}
+			Write-Host ""
+		}
+		elseif ($cmderStatus.contains("Blocked")) {
+			Write-Host '[Error] Cmder Install Blocked !!!'
+			$cmderDone = $true
+		}
+		elseif ($cmderStatus.contains("Failed")) {
+			Write-Host '[Error] Cmder Install Failed !!!'
+			$cmderDone = $true
+		}
 	}
-	else {
-		$Screen = Read-Host -Prompt 'Uninstall useless VSCode extensions ? (1/0)'
+
+	if ($RustDone -eq $false) {
+		
+		$RustStatus = (Get-Job -Name rustinstall) | Out-String
+	
+		if ($RustStatus.contains("Completed")) {
+			Write-Host '[Done] Rust Install'
+			$RustDone = $true
+		}
+		elseif ($RustStatus.contains("Running")) {
+			$text = '[Running] Rust Install'
+			for ($i = 0; $i -lt $text.Length; $i++) {
+				$c = $text[$i]
+		
+				Write-Host $c -NoNewline
+				Start-Sleep -Milliseconds 1
+			}
+			Write-Host ""
+		}
+		elseif ($RustStatus.contains("Blocked")) {
+			Write-Host '[Error] Rust Install Blocked !!!'
+			$RustDone = $true
+		}
+		elseif ($RustStatus.contains("Failed")) {
+			Write-Host '[Error] Rust Install Failed !!!'
+			$RustDone = $true
+		}
 	}
 
-	if ($Screen -eq "1") {
-		code --uninstall-extension formulahendry.dotnet-test-explorer
-		code --uninstall-extension formulahendry.dotnet
-		code --uninstall-extension ms-dotnettools.vscode-dotnet-runtime
-		code --uninstall-extension mikael.angular-beastcode
-		code --uninstall-extension johnpapa.angular-essentials
-		code --uninstall-extension angular.ng-template
-		code --uninstall-extension johnpapa.angular2
-		code --uninstall-extension bencoleman.armview
-		code --uninstall-extension rahulsahay.csharp-aspnetcore
-		code --uninstall-extension temilaj.asp-net-core-vs-code-extension-pack
-		code --uninstall-extension schneiderpat.aspnet-helper
-		code --uninstall-extension ms-vscode.azure-account
-		code --uninstall-extension msazurermtools.azurerm-vscode-tools
-		code --uninstall-extension ms-vscode.vscode-node-azure-pack
-		code --uninstall-extension ms-dotnettools.csharp
-		code --uninstall-extension thekalinga.bootstrap4-vscode
-		code --uninstall-extension artlaman.chalice-color-theme
-		code --uninstall-extension artlaman.chalice-icon-theme
-		code --uninstall-extension msjsdiag.debugger-for-chrome
-		code --uninstall-extension firefox-devtools.vscode-firefox-debug
-		code --uninstall-extension msjsdiag.debugger-for-edge
-		code --uninstall-extension hediet.vscode-drawio
-		code --uninstall-extension editorconfig.editorconfig
-		code --uninstall-extension ms-ceintl.vscode-language-pack-fr
-		code --uninstall-extension eamodio.gitlens
-		code --uninstall-extension ms-vscode.hexeditor
-		code --uninstall-extension ecmel.vscode-html-css
-		code --uninstall-extension heaths.vscode-guid
-		code --uninstall-extension ritwickdey.liveserver
-		code --uninstall-extension pkief.material-icon-theme
-		code --uninstall-extension eg2.vscode-npm-script
-		code --uninstall-extension jmrog.vscode-nuget-package-manager
-		code --uninstall-extension nrwl.angular-console
-		code --uninstall-extension johnpapa.vscode-peacock
-		code --uninstall-extension jebbs.plantuml
-		code --uninstall-extension esbenp.prettier-vscode
-		code --uninstall-extension ms-python.python
-		code --uninstall-extension ms-vscode-remote.remote-containers
-		code --uninstall-extension ms-vscode-remote.remote-ssh
-		code --uninstall-extension ms-vscode-remote.vscode-remote-extensionpack
-		code --uninstall-extension ms-vscode-remote.remote-wsl
-		code --uninstall-extension jock.svg
-		code --uninstall-extension hbenl.vscode-test-explorer
-		code --uninstall-extension darfka.vbscript
-		code --uninstall-extension johnpapa.winteriscoming
+	if ($VScodeDone -eq $false) {
 
-		code --install-extension dsznajder.es7-react-js-snippets
-		code --install-extension vscode-icons-team.vscode-icons
-		code --install-extension redhat.java
-		code --install-extension react.vscode-xml
-		code --install-extension ritwickdey.liveserver
-		Write-Host '[Done] Uninstall useless VSCode extensions'
+		$VScodeStatus = (Get-Job -Name removeVS) | Out-String
+
+		if ($VScodeStatus.contains("Completed")) {
+			Write-Host '[Done] Remove useless VScode extensions'
+			$VScodeDone = $true
+		}
+		elseif ($VScodeStatus.contains("Running")) {
+			$text = '[Running] Remove useless VScode extensions'
+			for ($i = 0; $i -lt $text.Length; $i++) {
+				$c = $text[$i]
+		
+				Write-Host $c -NoNewline
+				Start-Sleep -Milliseconds 1
+			}
+			Write-Host ""
+		}
+		elseif ($VScodeStatus.contains("Blocked")) {
+			Write-Host '[Error] Remove useless VScode extensions !!!'
+			$VScodeDone = $true
+		}
+		elseif ($VScodeStatus.contains("Failed")) {
+			Write-Host '[Error] Remove useless VScode extensions !!!'
+			$VScodeDone = $true
+		}
 	}
-	else {
-		Write-Host '[Skipped] Uninstall useless VSCode extensions'
+
+	if ($gitDone -eq $false) {
+
+		$gitStatus = (Get-Job -Name gitsetup) | Out-String
+
+		if ($gitStatus.contains("Completed")) {
+			."C:\Program Files (x86)\gnupg\bin\gpg.exe" --import \\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\keypublic.txt
+			Write-Host "[Done] Imported Git public key into gpg"
+			."C:\Program Files (x86)\gnupg\bin\gpg.exe" --import \\laboratoire.collegeem.qc.ca\Stockage\usagers\Etudiants\2032422\key.txt
+			Write-Host "[Done] Imported Git private key into gpg"
+
+			git config --global user.signingkey 881B04F1BF8134372145954483654F9843F106ED
+			git config --global commit.gpgsign true
+			git config --global gpg.program "c:/Program Files (x86)/GnuPG/bin/gpg.exe"
+			Write-Host '[Done] Setup git private key'
+			$gitDone = $true
+		}
+		elseif ($gitStatus.contains("Running")) {
+			$text = '[Running] Setup git private key'
+			for ($i = 0; $i -lt $text.Length; $i++) {
+				$c = $text[$i]
+		
+				Write-Host $c -NoNewline
+				Start-Sleep -Milliseconds 1
+			}
+			Write-Host ""
+		}
+		elseif ($gitStatus.contains("Blocked")) {
+			Write-Host '[Error] Setup git private key !!!'
+			$gitDone = $true
+		}
+		elseif ($gitStatus.contains("Failed")) {
+			Write-Host '[Error] Setup git private key !!!'
+			$gitDone = $true
+		}
 	}
-}
-else {
-	Write-Host '[Skipped] Uninstall useless VSCode extensions'
+
+	if ($VScodeDone -eq $true -and $cmderDone -eq $true -and $RustDone -eq $true -and $gitDone -eq $true) {
+		Write-Host '[Done] All tasks completed.'
+		$jobsNotDone = $false
+	}
+
+	Start-Sleep -s 1
 }
 #endregion
 
-#region Install Cmder
-if ($AlwaysSkipCmderInstall -eq "0") {
 
-	$Screen = "0"
 
-	if ($AlwaysInstallCmder -eq "1") {
-		$Screen = "1"
-	}
-	else {
-		$Screen = Read-Host -Prompt 'Install Cmder ? (1/0)'
-	}
-
-	if ($Screen -eq "1") {
-		Copy-Item -Recurse "Z:\ExtraSoftware\cmder" "C:\cmder"
-		Write-Host '[Done] Install Cmder'
-	}	
-	else {
-		Write-Host '[Skipped] Install Cmder'
-	}
-}
-else {
-	Write-Host '[Skipped] Install Cmder'
-}
-#endregion
